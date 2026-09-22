bc264016b48eb33e1387ab6263e6add11c9f8752
Alexis Nardiello <alexisnardiello@gmail.com>
2026-09-22 22:24:29 +0200

    Update

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 source .cod4env
 
 cd "$GAME"
-./cod4x18_dedrun$BIN_PREFIX +set fs_basepath "$GAME" +set fs_homepath "$GAME" +set fs_game "mods/$NAME" +set net_port 28960 +set developer 0 +set gscunit 0 +exec server.cfg +map mp_dr_shipment
+./cod4x18_dedrun$BIN_PREFIX +set fs_basepath "$GAME" +set fs_homepath "$GAME" +set fs_game "mods/$NAME" +set net_port 28960 +set developer 0 +set gscunit 0 +set sv_noauth 1 +exec server.cfg +map mp_dr_shipment
